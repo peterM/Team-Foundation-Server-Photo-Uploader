739951e220057db7d21ce0ec528b6cfd284f139e
Ing. Peter Malík - SL <3rror.svk@gmail.com>
2016-12-02 23:17:37 +0100

    PR 50: Add license header
     - Add license header
    
    Related work items: #839

--- Source/MalikP.TFS.PhotoUploader/MalikP.TFS.PhotoUploader/Program.cs
+++ Source/MalikP.TFS.PhotoUploader/MalikP.TFS.PhotoUploader/Program.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿//-------------------------------------------------------------------------------------------------
+// <copyright file="program.cs" company="MalikP.">
+//   Copyright (c) 2016-2017, Peter Malik.
+//   Authors: Peter Malik (MalikP.) (peter.malik@outlook.com)
+//
+//   Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+//   you may not use this file except in compliance with one of the Licenses.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//   or
+//       http://www.microsoft.com/opensource/licenses.mspx
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.TeamFoundation.Framework.Client;
